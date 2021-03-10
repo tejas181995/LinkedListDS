8388c264d5eaeed76c38c12aa4817dfa46610857
tejas181995 <tejasdev92@gmail.com>
2021-03-10 18:17:10 +0530

    Added inset method

--- src/main/java/com/bridgelabz/linkedlist/LinkedListDemo.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListDemo.java
@@ -2,16 +2,11 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListDemo {
     public static void main(String[] args) {
-        LinkedList<Integer> list= new LinkedList(30);
-        list.pushFront(70);
-        list.pushFront(56);
-//        list.insert(2,10);
-//        list.printList();
-//        Integer val = list.pop();
-//        System.out.println("val: " + val)
-        int val = list.popLast();
-        System.out.println("last pop: " + val);
-        System.out.println( list.indexOf(70));
+        LinkedList<Integer> list= new LinkedList(56);
+        list.pushBack(30);
+        list.pushBack(70);
+        list.printList();
+        list.insert(2, 40);
         list.printList();
 
     }
